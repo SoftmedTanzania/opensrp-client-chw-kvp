9a24f63edf4076ee45db3c34ac9458cc9f832bb9
Billy <billypatrick767@gmail.com>
2022-10-16 20:30:05 +0300

    fix: getting client's dominant group

--- opensrp-chw-kvp/src/main/java/org/smartregister/chw/kvp/dao/KvpDao.java
+++ opensrp-chw-kvp/src/main/java/org/smartregister/chw/kvp/dao/KvpDao.java
@@ -336,4 +336,17 @@ public class KvpDao extends AbstractDao {
         }
         return "";
     }
+
+    public static String getDominantKVPGroup(String baseEntityId) {
+        String sql = "SELECT client_group FROM ec_kvp_register p " +
+                " WHERE p.base_entity_id = '" + baseEntityId + "' AND p.is_closed = 0 ";
+
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "client_group");
+
+        List<String> res = readData(sql, dataMap);
+        if(res != null && res.size() != 0 && res.get(0)!= null){
+            return res.get(0);
+        }
+        return "";
+    }
 }
